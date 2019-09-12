cec67ba5b437bad21c0ff0c55f8279b0664972d8
Matěj Beran <matej.beran96@gmail.com>
2019-09-12 20:12:29 +0200

    Make enemies shoot.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,16 +4,43 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject laser;
     [SerializeField] float health = 500f;
+    [SerializeField] float minTimeBetweenSots = 0.2f;
+    [SerializeField] float maxTimeBetweenShots = 3f;
+    [SerializeField] float projectileSpeed = 5f;
+
+    float shotCounter;
 
     void Start()
     {
-
+        ResetShotCounter();
     }
 
     void Update()
     {
+        CountDownAndShoot();
+    }
 
+    private void ResetShotCounter()
+    {
+        shotCounter = Random.Range(minTimeBetweenSots, maxTimeBetweenShots);
+    }
+
+    private void CountDownAndShoot()
+    {
+        shotCounter -= Time.deltaTime;
+        if (shotCounter <= 0f)
+        {
+            Fire();
+            ResetShotCounter();
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject laserCopy = Instantiate(laser, transform.position, Quaternion.identity) as GameObject;
+        laserCopy.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, -projectileSpeed);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
