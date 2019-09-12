57d0d96c268ff4e570e810ec5e69f2875bde90f0
Matěj Beran <matej.beran96@gmail.com>
2019-09-12 20:59:16 +0200

    Add moving background and creating more prefabs.

---
+++ Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    [SerializeField] float backgroundScrollSpeed = 0.2f;
+
+    Material myMaterial;
+    Vector2 offset;
+
+    void Start()
+    {
+        myMaterial = gameObject.GetComponent<Renderer>().material;
+        offset = new Vector2(0f, backgroundScrollSpeed);
+    }
+
+    void Update()
+    {
+        myMaterial.mainTextureOffset += offset * Time.deltaTime;
+    }
+}
