ccbd6ea2803923911b9a9bc33d0982d76fb2c2e4
Matěj Beran <matej.beran96@gmail.com>
2019-09-12 18:39:54 +0200

    Add health to the enemy.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float health = 500f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var damageDealer = other.gameObject.GetComponent<DamageDelaer>();
+        if (null != damageDealer)
+        {
+            health -= damageDealer.GetDamage();
+        }
+    }
+}
