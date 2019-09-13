fa00d000ca2549be106050bf36957ae8599270a9
Matěj Beran <matej.beran96@gmail.com>
2019-09-13 21:03:30 +0200

    Add damage to player/enemy collision and delay scene loading when player dies.

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class Level : MonoBehaviour
 {
+    [SerializeField] int delayInSeconds = 2;
+
     public void LoadStartMenu()
     {
         SceneManager.LoadScene(0);
@@ -16,11 +18,23 @@ public class Level : MonoBehaviour
     }
     public void LoadGameOver()
     {
-        SceneManager.LoadScene("Game Over");
+        StartCoroutine(DelayLevel("Game Over"));
     }
 
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    private IEnumerator DelayLevel(string sceneName)
+    {
+        yield return new WaitForSeconds(delayInSeconds);
+        SceneManager.LoadScene(sceneName);
+    }
+
+    private IEnumerator DelayLevel(int sceneNumber)
+    {
+        yield return new WaitForSeconds(delayInSeconds);
+        SceneManager.LoadScene(sceneNumber);
     }
 }
